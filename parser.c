a62720da9ccdc832a0f7e312693601a2c57a096d
Solomon <solomonuche@gmail.com>
2023-08-23 02:07:11 +0100

    shell project

---
+++ parser.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * parse_cmd - a function that Parses input Line into tokens
+ * @input:User Input To be Parsed
+ *
+ * Return: (Array Of Char Parsed)
+ */
+char **parse_cmd(char *input)
+{
+	char **tokens;
+	char *token;
+	int i, buffsize = BUFSIZE;
+
+	if (input == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(*token) * buffsize);
+	if (!tokens)
+	{
+		perror("hsh");
+		return (NULL);
+	}
+
+	token = _strtok(input, "\n ");
+	for (i = 0; token; i++)
+	{
+		tokens[i] = token;
+		token = _strtok(NULL, "\n ");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
+}
